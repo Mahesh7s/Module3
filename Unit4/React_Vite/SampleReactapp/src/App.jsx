7be72c546466153daae98122bd068ae946544b56
Mahesh Moka <mokamahesh77@gmail.com>
2025-05-25 12:02:00 +0530

    commiting the code for 1sta nd 2nd questions in the assignment

---
+++ Unit4/React_Vite/SampleReactapp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './Components/Counter'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Counter number="20"/>
+    </>
+  )
+}
+
+export default App
